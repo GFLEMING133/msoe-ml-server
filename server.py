fe853571a72534efd9004e0ebe91289bd1fc0ce4
R. George Burkhardt <rgeorgebu@gmail.com>
2021-03-25 19:31:38 -0500

    Changed directory names to be more descriptive

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ app = Flask(__name__)
 
 @app.route('/get_mood_color_from_audio_file', methods=['POST'])
 def get_mood_color_from_audio_file():
-    return jsonify({'result': get_color_from_audio(request.files['audioSample'].read())})
+    result = get_color_from_audio(request.files['audioSample'].read())
+    with open('results.txt', 'a') as o:
+        o.write(result + '\n')
+    return jsonify({'result': result })
 
 @app.route('/get_mood_color_from_audio_stream', methods=['POST'])
 def get_mood_color_from_audio_stream():
-    return jsonify({'result': get_color_from_audio(request.get_data())})
+    result = get_color_from_audio(request.get_data())
+    with open('results.txt', 'a') as o:
+        o.write(result + '\n')
+    return jsonify({'result': result })
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
